fd74526081ca0339cec0f7484ae0f50f1a85bf23
Maxime MÉDAL <medal.max@gmail.com>
2024-04-10 14:16:36 +0200

    Add ComputeMultipleSum and RunAndExecute

---
+++ CodinGame_Wa/ComputeMultipleSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame_WA
+{
+    public class ComputeMultipleSum
+    {
+        public static int ComputeMultiplesSum(int n)
+        {
+            var res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                {
+                    res += i;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ CodinGame_Wa/ExceptionRunAndExecute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame_WA
+{
+    public class ExceptionRunAndExecute
+    {
+        public void Run(Service s, Connection c)
+        {
+            try
+            {
+                s.SetConnection(c);
+                s.Execute();
+                c.Commit();
+            }
+            catch (Exception err)
+            {
+                c.Rollback();
+                throw err;
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+
+        /// Definition of a service
+        public interface Service
+        {
+            void Execute();
+            void SetConnection(Connection c);
+        }
+
+        /// Definition of a connection
+        public interface Connection
+        {
+            void Commit();
+            void Rollback();
+            void Close();
+        }
+    }
+}

6835947ba25fbe01e0de9dfc2b152e5af18eb3ff
Maxime MÉDAL <medal.max@gmail.com>
2024-04-10 15:47:41 +0200

    WIP ChangeMoney

--- App/Program.cs
+++ App/Program.cs
@@ -20,15 +20,26 @@ public class Program
         //var res = test.CountFrequencies(dataIn);
 
         // Top Stocks
-        string[] stocks = { "AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA"};
-        float[,] prices = {
-            {12.81f, 11.09f, 12.11f, 10.93f, 9.83f, 8.14f},
-            {10.34f, 10.56f, 10.14f, 12.17f, 13.1f, 11.22f},
-            {11.53f, 10.67f, 10.42f, 11.88f, 11.77f, 10.21f}
-        };
+        //string[] stocks = { "AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA"};
+        //float[,] prices = {
+        //    {12.81f, 11.09f, 12.11f, 10.93f, 9.83f, 8.14f},
+        //    {10.34f, 10.56f, 10.14f, 12.17f, 13.1f, 11.22f},
+        //    {11.53f, 10.67f, 10.42f, 11.88f, 11.77f, 10.21f}
+        //};
 
-        var topStocks = new TopStocks();
-        var test = topStocks.GetTopStocks2(stocks, prices);
+        //var topStocks = new TopStocks();
+        //var test = topStocks.GetTopStocks2(stocks, prices);
+
+        // change Money
+        long s = 10; // Change this value to perform other tests
+        Change m = ChangeCoin.OptimalChange(s);
+
+        Console.WriteLine("Coin(s)  2€: " + m.coin2);
+        Console.WriteLine("Bill(s)  5€: " + m.bill5);
+        Console.WriteLine("Bill(s) 10€: " + m.bill10);
+
+        long result = m.coin2 * 2 + m.bill5 * 5 + m.bill10 * 10;
+        Console.WriteLine("\nChange given = " + result);
 
     }
 }
---
+++ CodinGame_Wa/ChangeCoin.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame_WA
+{
+    // Do not modify Change
+    public class Change
+    {
+        public long coin2 = 0;
+        public long bill5 = 0;
+        public long bill10 = 0;
+        public long Total() => coin2 + bill5 + bill10;
+        public long TotalCount() => coin2 * 2 + bill5 * 5 + bill10 * 10;
+    }
+    //class ChangeBis : Change
+    //{
+    //    public long Total() => coin2 + bill5 + bill10;
+    //    public long TotalCount() => coin2 * 2 + bill5 * 5 + bill10 * 10;
+
+    //}
+
+    public static class ChangeCoin
+    {
+
+        // Do not modify this method​​​​​​‌​‌​‌‌‌‌​‌‌‌‌‌‌​​​​‌‌​‌​‌ signature
+        public static Change OptimalChange(long s)
+            {
+            var res = new Change();
+            decimal by10 = 0;
+            decimal by5 = 0;
+            decimal by2 = 0;
+            decimal newS = s;
+            decimal divBy10 = Math.Floor(s / 10m);
+            decimal divBy5 = Math.Floor(s / 5m);
+            decimal divBy2 = Math.Floor(s / 2m);
+
+            for (long i = s; i == 0; i++)
+            {
+
+            }
+
+            if (divBy10 > 0 || divBy5 > 0 || divBy2 > 0)
+                Console.WriteLine("input : {0}", s);
+            if (Math.Floor(newS / 10m) > 0)
+            {
+                res.bill10 = (int)Math.Floor(newS / 10m);
+                newS = s - (res.bill10 * 10m);
+                Console.WriteLine("if 10 :{0}", res.bill10 * 10m);
+            }
+            // rest = rest / 5;
+            if (Math.Floor(newS / 5m) > 0)
+            {
+                res.bill5 = (int)Math.Floor(newS / 5m);
+                newS = s - (res.bill5 * 5m) - (res.bill10 * 10m);
+                Console.WriteLine("if 5 :{0}", newS);
+            }
+            if (Math.Floor(newS / 2m) > 0)
+            {
+                res.coin2 = (int)Math.Floor(newS / 2m);
+                newS = newS - (res.bill5 * 5m) - (res.bill10 * 10m) - (res.coin2 * 2m);
+                Console.WriteLine("if 2 :{0}", newS);
+            }
+            Console.WriteLine("=> Total: {0}", res.TotalCount());
+            return s - res.TotalCount() == 0 ? res : null;
+        }
+    }
+}
+//La monnaie est correcte pour une somme de 10€
+//La monnaie est optimale pour une somme de 10€ (1*10)
+//<code>null</code> est retourné quand la somme vaut 1
+//Le programme rend correctement la monnaie quand la somme vaut 31
+//La monnaie est optimale pour une somme de 31€ (2*10 +5 + 3*2)
+//<code>null</code> est retourné quand la somme vaut 3
+//Le programme rend correctement la monnaie quand la somme vaut 8 (4 * 2)
+//Résultat correct et dans les temps avec 9223372036854775802€
+//Résultat correct et dans les temps avec 9223372036854775803€
+//La monnaie est optimale avec 9223372036854775803€
